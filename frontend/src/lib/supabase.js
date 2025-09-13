044b7b53d35cfc0528598437cab543bd6f9d328f
Abhinav Sharma <0011abhinav@gmail.com>
2025-09-13 11:58:01 +0530

    ENV variables addition

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cnkpxudugwmlvptpzdim.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNua3B4dWR1Z3dtbHZwdHB6ZGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1NzQ3MjUsImV4cCI6MjA3MzE1MDcyNX0.6wupJ1F8A8vxXsmwsQudC1QZXmG6k_yKd-ZbZft3wzE'
+// Get Supabase credentials from environment variables
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
+// Create and export the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey)
